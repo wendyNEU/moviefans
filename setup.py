d7a17bed6134edef56cda53fcca08d2df60383f5
wendyNEU <qiu.di@husky.neu.edu>
2016-05-11 14:01:22 -0400

    Set up python base structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Di Qiu',
+      version='1.0',
+      description='MovieFans',
+      author='Di Qiu',
+      author_email='qiu.di@husky.neu.edu',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['tornado','pymongo'],
+     )
